c18daff088873d46bf69177736766514978f85bb
MHammadYou <MHammadY2.0@gmail.com>
2021-10-08 23:36:55 +0500

    Added routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { testRoute } from "./routes";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.json({"msg": "Working!..."})
-})
+
+app.use(express.json());
+app.use('/', testRoute);
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
---
+++ src/routes/index.ts
@@ -0,0 +1,1 @@
+export { default as testRoute } from "./test-route"
