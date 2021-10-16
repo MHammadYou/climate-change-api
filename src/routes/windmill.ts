0419a3b17860d616e60402c9c9aec00067fca6b0
MHammadYou <MHammadY2.0@gmail.com>
2021-10-16 23:46:42 +0500

    Added basic windmill data

--- src/routes/windmill.ts
+++ src/routes/windmill.ts
@@ -1,11 +1,29 @@
 import { Router } from "express";
 
-
 const router = Router();
 
 
 router.get('/windmill', async (req, res) => {
-  res.send("Windmill");
+
+  // 402,000 kWh per month
+  const singleTurbineEnergy = 402000 * 12;
+
+  /*
+  * In 2019, total U.S. electricity generation by the electric power industry
+  * of 4.13 trillion kilowatthours (kWh) from all energy sources resulted in
+  * the emission of 1.72 billion metric tons—1.90 billion short tons—of
+  * carbon dioxide (CO2). This equaled about 0.92 pounds of CO2 emissions per kWh.
+  * */
+
+  const co2PerKwh = 0.92;
+
+
+  const data = {
+    energyInKWH: singleTurbineEnergy
+  }
+
+  res.json(data);
+
 })
 
 export default router;
