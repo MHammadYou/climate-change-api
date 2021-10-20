707a9a5cc83a6071643aab2a9bba0b444954d488
MHammadYou <MHammadY2.0@gmail.com>
2021-10-20 23:33:21 +0500

    Completed solar route

--- src/routes/solar-panels.ts
+++ src/routes/solar-panels.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
+import DataInterface from "../data"
 
 
 const router = Router();
 
 
 router.get('/solar', (req, res) => {
-  res.send("Solar Panels");
+
+  const SPEnergy1Year = 500; // kwh between 500 and 550
+  const co2PerKwh = 0.92;
+  const SPlessEmissions = co2PerKwh * SPEnergy1Year;
+  const totalSPAprx = 1028400000;
+
+
+  const data: DataInterface = {
+    lessCarbonEmissions: `~${Math.trunc(SPlessEmissions * totalSPAprx * 10)}MT`,
+    totalUnits: totalSPAprx,
+    percentage: 2
+  }
+
+  res.json(data);
 })
 
 
