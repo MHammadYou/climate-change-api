a9817237d6c69a4239f8a9ee9edef85e933af04d
MHammadYou <MHammadY2.0@gmail.com>
2021-10-24 23:40:49 +0500

    Completed geothermal route

--- src/routes/geothermal.ts
+++ src/routes/geothermal.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
+import DataInterface from "../data";
 
 
 const router = Router();
 
 
 router.get('/geothermal', (req, res) => {
-  res.send("GeoThermal");
+
+  const co2PerKwh = 0.92;
+  const totalEnergy = 16.7; // kwh
+
+
+
+  const data: DataInterface = {
+    lessCarbonEmissions: `~${Math.trunc((totalEnergy / co2PerKwh) * 10)}MT`,
+    totalUnits: 200,
+    percentage: 0.4
+  }
+
+  res.json(data);
 })
 
 
